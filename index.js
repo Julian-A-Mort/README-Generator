421d033c61d46ef45dde766bcf4d46ac36b8e52f
Julian <julian@wheresnick.com.au>
2023-10-22 15:14:31 +1100

    began function to write readme file

--- index.js
+++ index.js
@@ -56,10 +56,60 @@ const questions = [
         message: 'Please provide your email address:',
         validate: input => /\S+@\S+\.\S+/.test(input) ? true : 'Please enter a valid email address',
           },
+
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+        const licenseBadge = data.license !== 'None' ? `![License](https://img.shields.io/badge/license-${encodeURIComponent(data.license.replace(" ", "_"))}-blue.svg)` : '';
+
+
+        const readmeContent = `
+      # ${data.title}
+      
+      ## Description
+      ${data.description}s
+      
+      ## Table of Contents
+      * [Installation](#installation)
+      * [Usage](#usage)
+      * [License](#license)
+      * [Contributing](#contributing)
+      * [Tests](#tests)
+      * [Questions](#questions)
+      
+      ## Installation
+      \`\`\`
+      ${data.installation}
+      \`\`\`
+      
+      ## Usage
+      ${data.usage}
+      
+      ## License
+      This project is licensed under the ${data.license} license.
+      
+      ## Contributing
+      ${data.contributing}
+      
+      ## Tests
+      To run tests, run the following command:
+      \`\`\`
+      ${data.tests}
+      \`\`\`
+      
+      ## Questions
+      If you have any questions about the repo, open an issue or contact [${data.username}](https://github.com/${data.username}) directly at ${data.email}.
+      `;
+
+      fs.writeFile(fileName, readmeContent, (err) => {
+        if (err) {
+          console.error('There was an error writing the README file:', err);
+        } else {
+          console.log('README file created successfully!');
+        }
+      });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
